9bc6a8b2855bf6b85896929db89383bd73474953
Tracey Zavadil <shinybirdy@yahoo.com>
2016-10-11 14:42:53 -0500

    got all links fixed i am awesome

--- server/app.js
+++ server/app.js
@@ -12,18 +12,17 @@ app.use(express.static("public"));
 
 app.get('/', function(req,res){
   console.log('in base url---sweeeeeet');
-res.sendFile(path.resolve('views/index.html'));
+res.sendFile(path.resolve('public/views/index.html'));
 
 });
 
 //app.set('views', path.join(__dirname, 'views'));
 
-
+app.listen( 3000 , function(){
+  console.log("Mama is listening on 3000...");
+});
 //app.use("/", routes);
 //app.use("/users", users );
 // ^^The first parameter is the path, the second one is the function to execute.
 //We separate the routes from the app.js because we don't want our app to be one big mess.
 //Separating files in Node makes use of module.exports.
-app.listen( 3000 , 'localhost', function(){
-  console.log("Mama is listening on 3000...");
-});
