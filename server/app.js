22f2bc5448fcf209a90e06d02f8d44407cad2980
Tracey Zavadil <shinybirdy@yahoo.com>
2016-10-11 14:29:05 -0500

    express whomp

--- server/app.js
+++ server/app.js
@@ -7,7 +7,16 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.set('views', path.join(__dirname, 'views'));
+//serves static files in public folder
+app.use(express.static("public"));
+
+app.get('/', function(req,res){
+  console.log('in base url---sweeeeeet');
+res.sendFile(path.resolve('views/index.html'));
+
+});
+
+//app.set('views', path.join(__dirname, 'views'));
 
 
 //app.use("/", routes);
@@ -15,31 +24,6 @@ app.set('views', path.join(__dirname, 'views'));
 // ^^The first parameter is the path, the second one is the function to execute.
 //We separate the routes from the app.js because we don't want our app to be one big mess.
 //Separating files in Node makes use of module.exports.
-
-app.get('/', function(req,res, next){
-  console.log('in base url---sweeeeeet');
-  res.sendFile( path.resolve( './public/views/index.html' ));
-});
-
-
-
-//serves static files in public folder
-
-app.use(express.static(path.join(__dirname, 'public')));
-//var index = require('./routes/index');
-// var masterSchedule = require("./routes/master_schedule");
-// var register = require('./routes/register');
-// var studentRoute = require("./routes/studentRoute");
-// //var topSecret = require('./routes/topsecret');
-// var user = require("./routes/user");
-//var ingredients = require("/routes/ingredients");
-
-//app.use('/ingredients', ingredients);
-// app.use('/nutrition', nutrition);
-// app.use('/processing', processing);
-// app.use('/production', production);
-// app.use('/storage', storage);
-
-app.listen( 3000 , function(){
+app.listen( 3000 , 'localhost', function(){
   console.log("Mama is listening on 3000...");
 });
