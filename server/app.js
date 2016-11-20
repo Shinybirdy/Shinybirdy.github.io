d0e318744a33da29bd40b6c8ca5123c063d9c30a
TraceyZavadil <traceyzavadil@gmail.com>
2016-11-19 23:36:46 -0600

    css elements

--- server/app.js
+++ server/app.js
@@ -5,23 +5,37 @@ var router = express.Router();
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static (__dirname + "/public"));
 
-app.get('/', function(req,res){
-  console.log('in base url---sweeeeeet');
-res.sendFile(path.resolve("/"));
+//=============================================================
+//SET PORT
+var port = process.env.PORT || 3000;
 
+// ROUTES FOR API
+// ============================================================
+var router = express.Router(); //gets instance of express router
+router.get('/', function(req,res){
+  res.json({message:"hello!"});
 });
+
+// MORE ROUTES TO GO HERE
+
+// ==============================================================
+// REGISTER ROUTES
+app.use('/api', router);
+
+
+// app.get('/', function(req,res){
+//   console.log('in base url---sweeeeeet');
+// res.sendFile(path.resolve("/"));
+// 
+// });
 
 //app.set('views', path.join(__dirname, 'views'));
 //serves static files in public folder
 
-app.listen( 3000 , function(){
-  console.log("Mama is listening on 3000...");
-});
-//app.use("/", routes);
-//app.use("/users", users );
-// ^^The first parameter is the path, the second one is the function to execute.
-//We separate the routes from the app.js because we don't want our app to be one big mess.
-//Separating files in Node makes use of module.exports.
+//========================================================
+// START THE SERVER
+app.listen(port);
+console.log("Mama is listening on 3000...");
--- server/public/scripts/myapp.js
+++ server/public/scripts/myapp.js
@@ -5,15 +5,15 @@ console.log("hey from myapp.js");
 app.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
   // Home
-   .when("/", {templateUrl: "views/home.html", controller: "PageCtrl"})
+   .when("/home", {templateUrl: "views/home.html", controller: "PageCtrl"})
   // Pages
-  .when("/ingredients", {templateUrl: "views/ingredients.html", controller: "PageCtrl"})
-  .when("/nutrition", {templateUrl: "views/nutrition.html", controller: "PageCtrl"})
-  .when("/processing", {templateUrl: "views/processing.html", controller: "PageCtrl"})
-  .when("/production", {templateUrl: "views/production.html", controller: "PageCtrl"})
-  .when("/recipes", {templateUrl: "views/recipes.html", controller: "PageCtrl"})
-  .when("/storage", {templateUrl: "views/storage.html", controller: "PageCtrl"})
-  .when("/about", {templateUrl: "views/about.html", controller: "PageCtrl"})
+  // .when("/ingredients", {templateUrl: "views/ingredients.html", controller: "PageCtrl"})
+  // .when("/nutrition", {templateUrl: "views/nutrition.html", controller: "PageCtrl"})
+  // .when("/processing", {templateUrl: "views/processing.html", controller: "PageCtrl"})
+  // .when("/production", {templateUrl: "views/production.html", controller: "PageCtrl"})
+  // .when("/recipes", {templateUrl: "views/recipes.html", controller: "PageCtrl"})
+  // .when("/storage", {templateUrl: "views/storage.html", controller: "PageCtrl"})
+  .when("/all_about_nutella", {templateUrl: "views/all_about_nutella.html", controller: "PageCtrl"})
 
 // else 404
   .otherwise("/404", {templateUrl: "views/404.html", controller: "PageCtrl"});
@@ -21,7 +21,7 @@ app.config(['$routeProvider', function ($routeProvider) {
 
 // Controls the Pages
 
-app.controller("formCtrl", function( $scope, $location, $http ){
-  $scope.recipe = {title:"Nutella Waffles", ingredients:""};
+app.controller("PageCtrl", function( $scope, $location, $http ){
+  // $scope.recipe = {title:"Nutella Waffles", ingredients:""};
   console.log("Page Controller having no control issues");
 });
