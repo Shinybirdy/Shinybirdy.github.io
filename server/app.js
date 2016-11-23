f1a0487f535abbf656ba8fd4610cad581bba63c2
TraceyZavadil <traceyzavadil@gmail.com>
2016-11-22 23:44:37 -0600

    tweaked express

--- server/app.js
+++ server/app.js
@@ -7,29 +7,41 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static (__dirname + "/public"));
-
+// app.use('/static', express.static(path.join(__dirname, 'public')));
 //=============================================================
 //SET PORT
 var port = process.env.PORT || 3000;
 
 // ROUTES FOR API
 // ============================================================
-var router = express.Router(); //gets instance of express router
+// var router = express.Router(); //gets instance of express router
+
 router.get('/', function(req,res){
-  res.json({message:"hello!"});
+  res.send("hey, home");
+
 });
+// home page route (http://localhost:8080)
+
+// about page route (http://localhost:8080/about)
+router.get('/home', function(req, res) {
+    res.send('im the home page!');
+});
+
+// apply the routes to our application
+app.use('/', router);
+
 
 // MORE ROUTES TO GO HERE
 
 // ==============================================================
 // REGISTER ROUTES
-app.use('/api', router);
+// app.use('/api', router);
 
 
 // app.get('/', function(req,res){
 //   console.log('in base url---sweeeeeet');
 // res.sendFile(path.resolve("/"));
-// 
+//
 // });
 
 //app.set('views', path.join(__dirname, 'views'));
